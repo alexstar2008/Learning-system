f8268032769192ed0332cf87c13eff09f5e7567e
alexstar2008 <alexstar2008@ukr.net>
2018-04-23 00:38:09 +0300

    port fix

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const fs = require('fs');
 //
+const coreRouter = require('./src/core/router');
 
 const app = new Koa();
-const router = new Router({
-    prefix: '/api'
-});
-app.use(router.routes());
-
 //
 const middlewareDir = `${__dirname}/src/middlewares`;
 const middlewares = fs.readdirSync(middlewareDir);
 middlewares.forEach(middleware => app.use(require(middlewareDir + '/' + middleware)()));
+//
+const router = new Router({
+    prefix: '/api'
+});
+router.use(coreRouter.routes());
+app.use(router.routes());
 
-module.exports = app.listen(3000, () => {
-    console.log(`Server is listening on port ${3000}`)
+
+module.exports = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is listening on port ${process.env.PORT || 3000}`)
 });
