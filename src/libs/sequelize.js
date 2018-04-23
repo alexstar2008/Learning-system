51fcb4311fb9a716807e79db904c34912556eee1
alexstar2008 <alexstar2008@ukr.net>
2018-04-23 16:13:25 +0300

    Implemented questions & themes

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -1,16 +1,35 @@
 const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 //
 const config = require('../../config');
-
+const db = {};
+//
 const sequelize = new Sequelize(config.db.url, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: true
     }
 });
-
 sequelize.authenticate().then(() => {
     console.log(`Postgres is started`);
 });
 
-module.exports = sequelize;
+const files =
+    fs.readdirSync(__dirname + '/../core/models')
+        .filter(file => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
+        .forEach(file => {
+            const model = sequelize['import'](path.join(__dirname + '/../core/models', file));
+            db[model.name] = model;
+        });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
